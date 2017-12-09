036fc302b7d0993a0c63b4e54803374a39e5792a
n4cl <devn4cl@gmail.com>
2017-12-10 04:08:42 +0900

    amountをprivateに更新
    amountをテストで使用しなくなったため

--- Dollar.py
+++ Dollar.py
@@ -4,14 +4,14 @@
 class Dollar(object):
 
     def __init__(self, amount):
-        self.amount = amount
+        self.__amount = amount
 
     def times(self, multiplier):
-        return Dollar(self.amount * multiplier)
+        return Dollar(self.__amount * multiplier)
 
     def equals(self, obj):
-        return self.amount == obj.amount
+        return self.__amount == obj.__amount
 
     def __eq__(self, other):
         # オブジェクトを比較した際には、このメソッドが呼ばれる
-        return self.amount == other.amount
+        return self.__amount == other.__amount
--- TODO.md
+++ TODO.md
@@ -1,7 +1,7 @@
 # TODO
 - $5 + 10CHF = $10 (2:1)
 - ~~$5 * 2 = $10~~
-- amountをprivateにする
+- ~~amountをprivateにする~~
 - ~~Dollarの副作用~~
 - Moneyの丸め処理
 - ~~equals()~~
