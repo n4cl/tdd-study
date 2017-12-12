a152a0ff20e3d0b4366bc2eb347d51903c9a285d
n4cl <devn4cl@gmail.com>
2017-12-13 02:42:00 +0900

    Dollarの一部をMoneyに移動

--- Dollar.py
+++ Dollar.py
@@ -6,14 +6,8 @@ from Money import Money
 class Dollar(Money):
 
     def __init__(self, amount):
-        self.__amount = amount
+        super(Dollar, self).__init__()
+        self._amount = amount
 
     def times(self, multiplier):
-        return Dollar(self.__amount * multiplier)
-
-    def equals(self, obj):
-        return self.__amount == obj.__amount
-
-    def __eq__(self, other):
-        # オブジェクトを比較した際には、このメソッドが呼ばれる
-        return self.__amount == other.__amount
+        return Dollar(self._amount * multiplier)
--- Money.py
+++ Money.py
@@ -2,4 +2,15 @@
 
 
 class Money(object):
-    pass
+
+    def __init__(self):
+        # TODO: Pythonで基底クラスでのインスタンス変数の宣言をどうすれば良いのか
+        # TODO: protectedが存在しないため、provateとする
+        self._amount = 0
+
+    def equals(self, obj):
+        return self._amount == obj._amount
+
+    def __eq__(self, other):
+        # オブジェクトを比較した際には、このメソッドが呼ばれる
+        return self._amount == other._amount
