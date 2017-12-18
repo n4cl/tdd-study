bcf50ed0ef2eb62343b1bcf1e7a7e737014eb0e3
n4cl <devn4cl@gmail.com>
2017-12-18 23:24:35 +0900

    Moneyにdollarを実装

--- Money.py
+++ Money.py
@@ -1,12 +1,19 @@
 # coding: utf-8
 
+from abc import ABCMeta, abstractmethod
+
 
 class Money(object):
+    __metaclass__ = ABCMeta
 
     def __init__(self):
         # TODO: Pythonで基底クラスでのインスタンス変数の宣言をどうすれば良いのか
         # TODO: protectedが存在しないため、provateとする
         self._amount = 0
+
+    @abstractmethod
+    def times(self, multiplier):
+        pass
 
     def equals(self, obj):
         return (self._amount   == obj._amount
@@ -16,3 +23,8 @@ class Money(object):
         # オブジェクトを比較した際には、このメソッドが呼ばれる
         return (self._amount   == other._amount
            and  self.__class__ == other.__class__)
+
+    @staticmethod
+    def dollar(amount):
+        from Dollar import Dollar
+        return Dollar(amount)
