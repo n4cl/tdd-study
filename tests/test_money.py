c989d8c5116a895fabedfd898e0a4da9642e302e
n4cl <devn4cl@gmail.com>
2017-12-19 00:24:33 +0900

    MoneyでFrancを返すようにテストを更新

--- tests/test_money.py
+++ tests/test_money.py
@@ -2,7 +2,6 @@
 
 import unittest
 from Money import Money
-from Franc import Franc
 
 
 class TestMoney(unittest.TestCase):
@@ -14,14 +13,14 @@ class TestMoney(unittest.TestCase):
     def test_equality(self):
         self.assertTrue(Money.dollar(5).equals(Money.dollar(5)))
         self.assertFalse(Money.dollar(5).equals(Money.dollar(6)))
-        self.assertTrue(Franc(5).equals(Franc(5)))
-        self.assertFalse(Franc(5).equals(Franc(6)))
-        self.assertFalse(Money.dollar(5).equals(Franc(5)))
+        self.assertTrue(Money.franc(5).equals(Money.franc(5)))
+        self.assertFalse(Money.franc(5).equals(Money.franc(6)))
+        self.assertFalse(Money.dollar(5).equals(Money.franc(5)))
 
     def test_franc_multiplication(self):
-        five = Franc(5)
-        self.assertEqual(Franc(10), five.times(2))
-        self.assertEqual(Franc(15), five.times(3))
+        five = Money.franc(5)
+        self.assertEqual(Money.franc(10), five.times(2))
+        self.assertEqual(Money.franc(15), five.times(3))
 
 if __name__ == "__main__":
     unittest.main()
